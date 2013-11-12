6cd2518644184748b7eadfe8d7090e2657e13284
jadielam@gmail.com <jadielam@gmail.com>
2013-11-12 13:19:21 -0700

    fourth commit

---
+++ src/algorithm/MapAlgorithm.java
@@ -0,0 +1,90 @@
+package algorithm;
+
+import java.util.HashSet;
+import java.util.List;
+
+import graph.Goal;
+import graph.Node;
+import graph.PrimitiveNode;
+import graph.DerivedNode;
+import graph.Edge;
+import graph.LFunction;
+import graph.DerivationNode;
+
+import objects.Conjunct;
+import objects.Fact;
+import objects.TraceStep;
+import graph.AttackGraph;
+
+public class MapAlgorithm {
+
+	
+	public MapAlgorithm(){
+		
+	}
+	
+	public static AttackGraph getGraph(HashSet<TraceStep> traceSteps, Goal g){
+		AttackGraph toReturn=null;
+		
+		//0. Creating the Builder to be used in the algorithm
+		Builder builder=new Builder();
+		
+		//1. Creating the structures that will make the graph
+		HashSet<DerivationNode> Nr=new HashSet<DerivationNode>();
+		HashSet<PrimitiveNode> Np=new HashSet<PrimitiveNode>();
+		HashSet<DerivedNode> Nd=new HashSet<DerivedNode>();
+		HashSet<Edge> E=new HashSet<Edge>();
+		LFunction L=new LFunction();
+		
+		//2. For each t in T
+		for (TraceStep t : traceSteps){
+			
+			//3. Create derivation node
+			DerivationNode r=builder.getDerivationNode();
+			Nr.add(r);
+			L.addEntry(r, t.getInteractionrule());
+			
+			//4. Look up n in Nd such that L(n)=Fact
+			Fact fact=t.getFact();
+			DerivedNode n=L.getDerivedNode(fact);
+			
+			//5. If such n does not exist, create a new one.
+			if (n==null){
+				n=builder.getDerivedNode();
+				L.addEntry(n, fact);
+			}
+			
+			//6. E<- E U {(n, r)}
+			Edge edge=new Edge(n, r);
+			E.add(edge);
+			
+			//7. For each fact f in Conjunct
+			Conjunct conjunct=t.getConjunct();
+			List<Fact> facts=conjunct.getFacts();
+			for (Fact f : facts){
+				Node c=L.getNode(f);
+				if (c==null){
+					
+					if (builder.isPrimitive(f)){
+						c=builder.getPrimitiveNode();
+						Np.add((PrimitiveNode)c);
+					}
+					else{
+						c=builder.getDerivedNode();
+						Nd.add((DerivedNode)c);
+					}
+					
+				}
+				Edge e=new Edge(r, c);
+				E.add(e);
+				
+			}
+			
+		}
+		
+		
+		toReturn=new AttackGraph(Nr, Np, Nd, E, L, g);
+		return toReturn;
+	}
+
+}
