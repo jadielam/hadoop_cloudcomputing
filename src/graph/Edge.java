075501a95b746c47bede739940024fb6b2b59eec
Jadiel <jadielam@gmail.com>
2013-11-12 10:54:55 -0700

    Update Edge.java

--- src/graph/Edge.java
+++ src/graph/Edge.java
@@ -12,6 +12,32 @@ public class Edge {
     
     
    private Node source;
+private Node destination;
+@Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 43 * hash + Objects.hashCode(this.source);
+        hash = 43 * hash + Objects.hashCode(this.destination);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Edge other = (Edge) obj;
+        if (!Objects.equals(this.source, other.source)) {
+            return false;
+        }
+        if (!Objects.equals(this.destination, other.destination)) {
+            return false;
+        }
+        return true;
+    }
 
     public Node getSource() {
         return source;
@@ -28,7 +54,7 @@ public class Edge {
     public void setDestination(Node destination) {
         this.destination = destination;
     }
-    private Node destination;
+    
     public Edge(Node src, Node dest){
     
     source=src;
