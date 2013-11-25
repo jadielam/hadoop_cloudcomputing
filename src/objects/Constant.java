cc770e4a5fd03f09306c080dc4fdfb5f4b0aa6c2
sharanyareddy <scharabu@asu.edu>
2013-11-24 21:37:35 -0700

    Update Constant.java

--- src/objects/Constant.java
+++ src/objects/Constant.java
@@ -8,66 +8,68 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 
 public class Constant implements Writable {
-	
-	private IntWritable id;
-	private Text text;
-	
-	public Constant(int id, String text) {
-		this.id = new IntWritable(id);
-		this.text = new Text(text);
-	}	
-	
-	public IntWritable getId() {
-		return id;
-	}
-	
-	public Text getText() {
-		return text;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Constant other = (Constant) obj;
-		if (id != other.id)
-			return false;
-		if (text == null) {
-			if (other.text != null)
-				return false;
-		} else if (!text.equals(other.text))
-			return false;
-		return true;
-	}
-	
-	public void write(DataOutput out) throws IOException{
-		id.write(out);
-		text.write(out);
-	}
-	
-	public void readFields(DataInput in) throws IOException {
-		id.readFields(in);
-		text.readFields(in);
-	}
-	 @Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		result = prime * result + ((text == null) ? 0 : text.hashCode());
-		return result;
-	}
-	 
-	 public int compareTo(Object other ) {  
-	    int result = 0;
-		return result;
-	  }  
+        
+        private IntWritable id;
+        private Text text;
+        public Constant()
+        {}
+        
+        public Constant(int id, String text) {
+                this.id = new IntWritable(id);
+                this.text = new Text(text);
+        }        
+        
+        public IntWritable getId() {
+                return id;
+        }
+        
+        public Text getText() {
+                return text;
+        }
+        
+        @Override
+        public boolean equals(Object obj) {
+                if (this == obj)
+                        return true;
+                if (obj == null)
+                        return false;
+                if (getClass() != obj.getClass())
+                        return false;
+                Constant other = (Constant) obj;
+                if (id != other.id)
+                        return false;
+                if (text == null) {
+                        if (other.text != null)
+                                return false;
+                } else if (!text.equals(other.text))
+                        return false;
+                return true;
+        }
+        
+        public void write(DataOutput out) throws IOException{
+                id.write(out);
+                text.write(out);
+        }
+        
+        public void readFields(DataInput in) throws IOException {
+                id.readFields(in);
+                text.readFields(in);
+        }
+         @Override
+        public int hashCode() {
+                final int prime = 31;
+                int result = 1;
+                result = prime * result + id.get();
+                result = prime * result + ((text == null) ? 0 : text.hashCode());
+                return result;
+        }
+         
+         public int compareTo(Object other ) {  
+            int result = 0;
+                return result;
+          }  
 
-	 public String toString(){
-		 return text;
-	 }
+         public String toString(){
+                 return text.toString();
+         }
 }
