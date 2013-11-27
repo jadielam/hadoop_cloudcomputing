80224d674280e837b7c896e95809d0b832019980
jadielam@gmail.com <jadielam@gmail.com>
2013-11-27 00:32:02 -0700

    adsf

--- src/mapreduce/ReduceClass.java
+++ src/mapreduce/ReduceClass.java
@@ -33,7 +33,7 @@ public class ReduceClass extends MapReduceBase implements Reducer<LongWritable, 
 		
 		ReduceAlgorithm reducer=new ReduceAlgorithm(graphs);
 		Graph reducedGraph=reducer.reduce();
-		reducedGraph.setNoGraphs(graphs.size());
+		reducedGraph.setNoGraphs(graphs.get(0).getNr().size());
 		
 		output.collect(key,  reducedGraph);
 		
