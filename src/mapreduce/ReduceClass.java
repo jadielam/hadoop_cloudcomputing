f483ee302874d82d72afd1252f8fbca2fe5c5d9e
jadielam@gmail.com <jadielam@gmail.com>
2013-11-27 01:54:11 -0700

    adsf

--- src/mapreduce/ReduceClass.java
+++ src/mapreduce/ReduceClass.java
@@ -5,12 +5,16 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.Map.Entry;
 
 import graph.AttackGraph;
 import graph.AttackGraphConverter;
 import algorithm.ReduceAlgorithm;
 
+import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reducer;
@@ -27,17 +31,36 @@ public class ReduceClass extends MapReduceBase implements Reducer<LongWritable, 
 		
 		List<Graph> graphs=new LinkedList<Graph>();
 		int counter=0;
+		
+		MapWritable NrFinal=new MapWritable();
+		MapWritable NpFinal=new MapWritable();
+		MapWritable NdFinal=new MapWritable();
+		MapWritable EFinal=new MapWritable();
+		Text goalFinal=new Text("");
+
 		while (values.hasNext()){
-			graphs.add(values.next());
+			Graph a=values.next();
+			reduceMap(NrFinal, a.getNr());
+			reduceMap(NpFinal, a.getNp());
+			reduceMap(NdFinal, a.getNd());
+			reduceMap(EFinal, a.getE());
 			if (counter==1) break;
 			counter++;
 		}
 		
 		
-		ReduceAlgorithm reducer=new ReduceAlgorithm(graphs);
-		Graph reducedGraph=reducer.reduce();
+		Graph reducedGraph=new Graph(NrFinal, NpFinal, NdFinal, EFinal, goalFinal);
 				
 		output.collect(key,  reducedGraph);
+		
+	}
+	
+	private void reduceMap(MapWritable m1, MapWritable m2){
+		
+		Set<Entry<Writable, Writable>> entrySet=m2.entrySet();
+		for (Entry<Writable, Writable> e : entrySet){
+			m1.put((Text)e.getKey(), (LongWritable)e.getValue());
+		}
 		
 	}
 
